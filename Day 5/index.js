173d99f6005bc13537eb06d136669ff8f2e04c89
Ayush-Sharma3011 <ayush.sharma235@lpu.in>
2024-12-29 23:42:19 +0530

    Day 5

---
+++ Day 5/index.js
@@ -0,0 +1,60 @@
+//alert("Welcome to our site");
+// Variable
+//var name='Shakil';
+//let name1='Ayush';
+//const name2="Pro";
+
+//console.log(name1 + name1 + name2);
+
+// let unit= 34;
+// let total_sum = 0;
+// if (unit<100) {
+//     total_sum +=unit*5;
+// } 
+// else if(100<unit<200){
+//     total_sum=(unit-100)*7 + 500;
+// }
+// else{
+//     total_sum = (unit-200) *10 + 1200;
+// }
+// console.log(total_sum);
+
+// let name1 = prompt('Enter ur name:');
+// alert(name1 + ' Welcome to our page!!');
+
+// let c=[1,2,3,4,5];
+// let d={
+//     name1: "John",
+//     age: 30,
+// };
+
+
+// Fetch
+
+// fetch("https://randomuser.me/api/")
+
+// .then((chithi) => {
+//     return chithi.json();
+// })
+
+// .then((data)=> {
+//     console.log(data.results[0].name.first);
+// })
+
+
+
+//Promises
+
+// const wada=new Promise((resolve, reject) => {
+//     axios.get("https://randomuser.me/api/")
+// .then((chithi)=>{
+//     resolve(chithi);
+// })
+// .catch((err)=>
+// {
+//     reject(err);
+// })
+// })
+// .then((chithi)=>{
+//     console.log(chithi);
+// })
